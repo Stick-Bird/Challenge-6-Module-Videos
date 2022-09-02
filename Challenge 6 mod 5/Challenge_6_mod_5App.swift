8a8fc411ae85e5a78ed2ffd4f7daec3679a658a0
Craig Butler <craigbutler123456@gmail.com>
2022-09-02 06:28:28 -0600

    Initial Commit

---
+++ Challenge 6 mod 5/Challenge_6_mod_5App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_6_mod_5App.swift
+//  Challenge 6 mod 5
+//
+//  Created by user on 2022-09-02.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_6_mod_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
